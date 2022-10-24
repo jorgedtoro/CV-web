81bda9dc42d37db887f637dff0f540df444d2a2e
Jorge de Toro <jorgedtoro@gmail.com>
2022-10-24 16:37:54 +0200

    experience design cards

--- CV2022/src/app/app-routing.module.ts
+++ CV2022/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 import { HeroComponent } from './components/hero/hero.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HeroComponent },
+  { path: 'viewExperience/:idExperience', component: ExperienceComponent },
   { path: 'contact', component: ContactComponent },
 ];
 
