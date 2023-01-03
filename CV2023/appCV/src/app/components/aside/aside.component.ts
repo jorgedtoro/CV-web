6cb3249bb7691d2aed715eaeeac9fa2887b00880
Jorge de Toro <72794927+jorgedtoro@users.noreply.github.com>
2023-01-03 19:14:28 +0100

    add aside responsive

--- CV2023/appCV/src/app/components/aside/aside.component.ts
+++ CV2023/appCV/src/app/components/aside/aside.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+  styleUrls: ['./aside.component.scss'],
 })
 export class AsideComponent implements OnInit {
+  mobileDisplay: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  show() {
+    this.mobileDisplay = !this.mobileDisplay;
+    console.log(this.mobileDisplay);
   }
-
 }
