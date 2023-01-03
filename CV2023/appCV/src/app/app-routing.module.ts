a1a995d12b0edc98b6cf10ac6b48e5df504e4c40
Jorge de Toro <72794927+jorgedtoro@users.noreply.github.com>
2023-01-03 16:38:13 +0100

    add hero content

--- CV2023/appCV/src/app/app-routing.module.ts
+++ CV2023/appCV/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ListProjectsComponent } from './components/list-projects/list-projects.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HeroComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ListProjectsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'experience', component: ExperienceComponent },
 ];
 
 @NgModule({
--- CV2023/appCV/src/app/app.module.ts
+++ CV2023/appCV/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { AsideComponent } from './components/aside/aside.component';
 import { AsideProfileComponent } from './components/aside-profile/aside-profile.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ListProjectsComponent } from './components/list-projects/list-projects.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AsideComponent,
     AsideProfileComponent,
-    HeroComponent
+    HeroComponent,
+    ContactComponent,
+    ListProjectsComponent,
+    EducationComponent,
+    ExperienceComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
